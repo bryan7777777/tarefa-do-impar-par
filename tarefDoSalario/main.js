2aaf1a1dca408b90a0805f40f99d7002c38784f9
bryan7777777 <bryannagano15@gmail.com>
2025-05-12 13:56:33 -0300

    add img

--- tarefDoSalario/main.js
+++ tarefDoSalario/main.js
@@ -5,10 +5,10 @@ function media() {
     salario = Number(prompt("digite seu salario"));
     vendas = Number(prompt("digite o valor das vendas"));
 
-    if (vendas < 3000) {
+    if (vendas > 3000) {
         comissao = 15;
     } else {
-        if (vendas <= 1500) {
+        if (vendas >= 1500) {
             comissao = 10;
         } else {
             comissao = 5;
@@ -17,7 +17,7 @@ function media() {
 
     // porcentagem é o numero que eu quero dividir vezes o divisor dividido por 100, ex 2000 X 15 / 100
 
-    comissaoValor = vendas / comissao;
+    comissaoValor = vendas * comissao / 100;
     pagamento = salario + comissaoValor;
 
     alert(`Vendedor: ${nome} \n ____________________ \n Valor das Vendas: ${vendas} \n Valor da Comissão: ${comissao} \n Valor do Salario: ${salario} \n \n Pagamento: ${pagamento}`);
